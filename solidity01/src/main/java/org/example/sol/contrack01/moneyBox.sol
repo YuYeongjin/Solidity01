ccb67350d58bda074b5ee4093663db8731dda4bc
유영진 <svvo2002@naver.com>
2022-12-23 09:00:54 +0900

    221223 moneybox

--- solidity01/src/main/java/org/example/sol/contrack01/moneyBox.sol
+++ solidity01/src/main/java/org/example/sol/contrack01/moneyBox.sol
@@ -1,15 +1,13 @@
 pragma solidity >=0.7.0 < 0.9.0;
 
+/*
+ 1. 1 이더만 내야함
+ 2. 중복해서 참여 불가 (적립금을 받으면 초기화)
+ 3. 관리자만 적립된 이더 볼 수 있음
+ 4. n 배수 번째 사람에게만 적립된 이더를 지급함.
+*/
+
 contract moneyBox {
-
-    /*
-     1. 1 이더만 내야함
-     2. 중복해서 참여 불가 (적립금을 받으면 초기화)
-     3. 관리자만 적립된 이더 볼 수 있음
-
-
-
-    */
     event whoPaid(address indexed sender, uint256 payment);
 
     address owners;
@@ -19,6 +17,9 @@ contract moneyBox {
 
     constructor(){
         owner = msg.sender;
+    }
+    receive() external payable{
+       require(msg.value == 1 ether, "Must be 1 ether.");
 
     }
 
