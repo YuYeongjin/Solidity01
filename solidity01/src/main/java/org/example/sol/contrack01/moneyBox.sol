9538a06e239cef5bf5e6b5520e2da801aa87fdd2
유영진 <svvo2002@naver.com>
2022-12-23 14:56:25 +0900

    221223 moneybox

--- solidity01/src/main/java/org/example/sol/contrack01/moneyBox.sol
+++ solidity01/src/main/java/org/example/sol/contrack01/moneyBox.sol
@@ -13,6 +13,9 @@ contract moneyBox {
     address owners;
 
     mapping (uint256 => mapping(address => bool)) paidMemberList;
+
+    mapping(address=>bool) paidMemberList2;
+
     uint256 round = 1;
 
     constructor(){
@@ -23,9 +26,18 @@ contract moneyBox {
         require(paidMemberList[round][msg.sender] == false, "Must be a new player in each game.");
         paidMemberList[round][msg.sender]=true;
         emit whoPaid(msg.sender, msg.value);
-        if(address (this).balance == 3ether){
-
+        if(address (this).balance == 3 ether){
+            (bool sent,)= payable(msg.sender).call{value:address(this).balance}("");
+            require(sent,"Failed to pay");
+            round++;
         }
+    }
+    function checkRound() public view returns(uint256){
+        return round;
+    }
+    function checkValue() public view returns(uint256){
+        require(owner==msg.sender,"Only Onwer can check the value");
+        return address(this).balance;
     }
 
 }
