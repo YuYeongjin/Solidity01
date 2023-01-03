e5d71114da5eb8e69c7bdc4b4595aeedfa028b39
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2023-01-03 13:46:40 +0900

    Update practice.sol

--- solidity01/src/main/java/org/example/sol/Ex9/practice.sol
+++ solidity01/src/main/java/org/example/sol/Ex9/practice.sol
@@ -3,14 +3,17 @@ pragma solidity >=0.7.0 < 0.9.0;
 
 contract pr1 {
     
-   // address payable myAcount;
+    address myAcount;
+    constructor() payable {}
+    function money(address _address) public payable returns(bool){
+        myAcount = _address;
+        (bool send) = payable(myAcount).send(msg.value);
+        return send;
+    }
 
-   // function money(address _address) public payable returns(bool){
-   //     myAcount = payable(_address);
-   //     (bool send,) = myAcount.transfer(msg.value);
-   //     return send;
-   // }
-
+    function getMoney() public view returns(uint){
+        return myAcount.balance;
+    }
 }
 contract Ex9_2 {
 
