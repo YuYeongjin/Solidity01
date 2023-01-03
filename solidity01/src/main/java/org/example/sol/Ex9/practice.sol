114dc80081a441ef5836fd0c323306a8c723989f
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2023-01-03 10:12:41 +0900

    Update practice.sol

--- solidity01/src/main/java/org/example/sol/Ex9/practice.sol
+++ solidity01/src/main/java/org/example/sol/Ex9/practice.sol
@@ -1,7 +1,23 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 < 0.9.0;
 
-contract practice {
-    function practice(){
+contract pr1 {
+    
+    address payable myAcount;
 
+    function money(address _address) public payable returns(bool){
+        myAcount = payable(_address);
+        (bool send,) = myAcount.transfer(msg.value);
+        return send;
     }
+
+}
+contract Ex9_2 {
+
+    constructor() payable {}
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
 }
