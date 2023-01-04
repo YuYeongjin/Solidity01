38b418e5a1f96bf77e421acbd330559804135ce6
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2023-01-04 16:46:19 +0900

    pickGame

--- solidity01/src/main/java/org/example/sol/Ex9/test1.sol
+++ solidity01/src/main/java/org/example/sol/Ex9/test1.sol
@@ -1,20 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 < 0.9.0;
+pragma solidity >= 0.7.0 < 0.9.0;
 
-contract ownerGame {
-   address owner;
-   uint money=msg.value;
-   constructor() public{
-       owner=msg.sender;
-   }
-   modifier onlyOwner(){
-       require(owner == msg.sender,"Only owner play game");
-       _;
-   }
-      
-   function play() public onlyOwner() returns (bool){
-       require(money==10*15, "Need 0.1ether");
-       (bool sent )= payable(msg.sender).send(money);
-       return true;
-   }
+contract pickGame {
+    address owner;
+    uint256 inputEth=msg.value;
+    constructor() public{
+        owner = msg.sender;
+    }
+    modifier onlyOwner(){
+        require(msg.sender==owner,"Create only owner");
+        _;
+    }
+
+    uint256[5][5] pickGameBoard;
+
+    
+
 }
