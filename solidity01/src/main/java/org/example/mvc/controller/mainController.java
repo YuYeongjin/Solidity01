9583f8ab0dc5e4f37d05995164a36c627ae5bd3e
user <svvo2002@naver.com>
2023-01-04 23:48:42 +0900

    20230104 smart contract 개발

--- solidity01/src/main/java/org/example/mvc/controller/mainController.java
+++ solidity01/src/main/java/org/example/mvc/controller/mainController.java
@@ -1,12 +1,13 @@
-package org.example.block.controller;
+package org.example.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class mainController {
-    @RequestMapping("/")
-    public void home(){
+    @RequestMapping("")
+    public String home(){
+        return "redirect:/home";
     }
 
 }
