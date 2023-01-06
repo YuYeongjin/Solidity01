a8ca7d4db752b39345f7b9f2fd417046ac1ab0d5
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2023-01-06 14:01:59 +0900

    20230106 3회 입금 시 인출가능

--- solidity01/src/main/java/org/example/solidity/contrack01/Installment.sol
+++ solidity01/src/main/java/org/example/solidity/contrack01/Installment.sol
@@ -5,18 +5,41 @@ contract Installment {
     // 3회 적금 시 적금 총액을 돌려받기
     address owner;
     uint[] account;
-    uint count;
+    uint256 count;
+    uint256 money;
     constructor() {
         owner = msg.sender;
     }
     
     modifier onlyOwner(){
         require(msg.sender==owner,"only owner");
+        require(msg.value>=0,"Input Ether");
         _;
+        count++;
     }
 
-    function installment() public onlyOwner() {
-        account.push(msg.value);
+    function howMuch() internal {
+        money = msg.value;
+    }
+
+    function installment() public onlyOwner() payable{
+        howMuch();
+        account.push(money);
+    }
+    function withdraw() public onlyOwner() payable returns(bool){
+        require(count==3,"More installment require");
+        
+        (bool success,)= payable(msg.sender).call{value:address(this).balance}("");
+        require (success,"Send false");
+        count =0;
+        return success;
+    }
+
+    function howMuchMoney() public view returns(uint){
+        return address(this).balance;
+    }
+    function howManyTime() public view returns(uint){
+        return count;
     }
 
 }
